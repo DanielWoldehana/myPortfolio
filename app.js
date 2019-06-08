7a0780a0474ba74cb51139652106428066404c90
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-06-07 20:36:06 -0400

    contact form complete

--- app.js
+++ app.js
@@ -1,57 +1,96 @@
-let aboutIcon = document.querySelectorAll(".aboutIcon")
-let profilePic = document.querySelector('.profilePic')
-let ctx = document.getElementById('myChart');
+let aboutIcon = document.querySelectorAll(".aboutIcon");
+let profilePic = document.querySelector(".profilePic");
+let ctx = document.getElementById("myChart");
 
 Chart.defaults.global.animation.duration = 10000;
 
+let mySubmit = document.querySelector(".submit");
+mySubmit.addEventListener("click", function(evt) {
+  console.log("clicked");
+  let email = document.querySelector(".email").value;
+  let subject = document.querySelector(".subject").value;
+  let message = document.querySelector(".message").value;
+  let status = document.querySelector(".status");
+  status.innerHTML = "";
+  let p = document.createElement("p");
+  let p2 = document.createElement("p");
+  let p3 = document.createElement("p");
+  if (email.length < 5 && !email.includes("@") && !email.includes(".")) {
+    evt.preventDefault();
+    p.setAttribute("style", "color: red;");
+    p.innerHTML = "Email is not Valid";
+    status.appendChild(p);
+  }
+
+  if (subject.length < 2) {
+    evt.preventDefault();
+    p2.setAttribute("style", "color: red;");
+    p2.innerHTML = "Subject is not Valid";
+    status.appendChild(p2);
+  }
+
+  if (message.length < 10) {
+    evt.preventDefault();
+    p3.setAttribute("style", "color: red;");
+    p3.innerHTML = "Message is not Valid";
+    status.appendChild(p3);
+  }
+});
 
 // @ts-ignore
 let myChart = new Chart(ctx, {
-  type: 'horizontalBar',
+  type: "horizontalBar",
   data: {
-    labels: ['HTML', 'CSS', 'JavaScript', 'React', 'MongoDB', 'Express', 'Python'],
-    datasets: [{
-      label: 'languages',
-      data: [
-        93,
-        90,
-        85,
-        70,
-        70,
-        65,
-        50,
-        0,
-        100
-      ],
-      // backgroundColor: '#243139',
-      backgroundColor: ['rgba(68, 68, 68, 0.6)', 'rgba(0, 143, 199, 0.6)', 'rgba(83, 178, 143, 0.6)', 'rgba(255, 179, 15, 0.6)', 'rgba(255, 105, 5, 0.6)', 'rgb(245, 20, 0, 0.6)', 'rgba(171, 217, 102, 0.6)'],
-      borderWidth: 2,
-      borderColor: '#999',
-      hoverBorderWidth: 3,
-      hoverBorderColor: 'black'
-    }]
+    labels: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "MongoDB",
+      "Express",
+      "Python"
+    ],
+    datasets: [
+      {
+        label: "languages",
+        data: [93, 90, 85, 70, 70, 65, 50, 0, 100],
+        // backgroundColor: '#243139',
+        backgroundColor: [
+          "rgba(68, 68, 68, 0.6)",
+          "rgba(0, 143, 199, 0.6)",
+          "rgba(83, 178, 143, 0.6)",
+          "rgba(255, 179, 15, 0.6)",
+          "rgba(255, 105, 5, 0.6)",
+          "rgb(245, 20, 0, 0.6)",
+          "rgba(171, 217, 102, 0.6)"
+        ],
+        borderWidth: 2,
+        borderColor: "#999",
+        hoverBorderWidth: 3,
+        hoverBorderColor: "black"
+      }
+    ]
   },
   options: {}
+});
 
-})
+profilePic.addEventListener("mouseover", function() {
+  profilePic.classList.add("scaleClass");
+});
 
-profilePic.addEventListener('mouseover', function () {
-  profilePic.classList.add('scaleClass')
-})
+profilePic.addEventListener("mouseout", function() {
+  profilePic.classList.remove("scaleClass");
+});
 
-profilePic.addEventListener('mouseout', function () {
-  profilePic.classList.remove('scaleClass')
-})
-
-aboutIcon.forEach(function (elem) {
-  elem.addEventListener("mouseover", function () {
-    elem.classList.add('spinClass')
+aboutIcon.forEach(function(elem) {
+  elem.addEventListener("mouseover", function() {
+    elem.classList.add("spinClass");
   });
 });
 
-aboutIcon.forEach(function (elem) {
-  elem.addEventListener("mouseout", function () {
-    elem.classList.remove('spinClass')
+aboutIcon.forEach(function(elem) {
+  elem.addEventListener("mouseout", function() {
+    elem.classList.remove("spinClass");
   });
 });
 
@@ -84,7 +123,6 @@ if (window.innerWidth >= 1100) {
       ease: Power1.easeInOut
     })
   );
-
 
   // @ts-ignore
   const controller = new ScrollMagic.Controller();
